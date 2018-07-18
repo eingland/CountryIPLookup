50d6ad15088cec7a0061750325b3899585894b2a
Eric Ingland <eingland@users.noreply.github.com>
2018-07-17 20:42:04 -0500

    Add country code lookup.

--- CountryIPLookup/Pages/Index.cshtml.cs
+++ CountryIPLookup/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +11,52 @@ namespace CountryIPLookup.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        public IPModel IP { get; set; }
+
         public void OnGet()
         {
 
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await GetCountryCode(IP.Address);
+
+            return Page();
+        }
+
+        public async Task GetCountryCode(string address)
+        {
+            await Task.Run(() => {
+                var lines = System.IO.File.ReadAllLines(Path.Combine("Assets", "dbip-country-2018-07.csv")).Select(a => a.Split(';'));
+                var csv = from line in lines
+                          select (line[0].Split(',')).ToArray();
+                var code = from range in csv
+                           where ConvertIPToUint(range[0]) > ConvertIPToUint(address) && ConvertIPToUint(range[1]) < ConvertIPToUint(address)
+                           select (range[2]);
+                IP.CountryCode = code.First().Replace("\"", "");
+            });
+        }
+
+        static public uint ConvertIPToUint(string ipAddress)
+        {
+            ipAddress = ipAddress.Replace("\"", "");
+            System.Net.IPAddress iPAddress = System.Net.IPAddress.Parse(ipAddress);
+            byte[] byteIP = iPAddress.GetAddressBytes();
+            uint ipInUint = (uint)byteIP[3] << 24;
+            ipInUint += (uint) byteIP[2] << 16;
+            ipInUint += (uint) byteIP[1] << 8;
+            ipInUint += (uint) byteIP[0];
+            return ipInUint;
+        }
+}
+
+    public class IPModel
+    {
+        [Required]
+        public string Address { get; set; }
+        public string CountryCode { get; set; }
     }
 }
